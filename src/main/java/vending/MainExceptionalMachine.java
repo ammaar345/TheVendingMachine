44cd3209771572f1ecd971fa3ae30723893397cd
ammaar <fakirammaar8@gmail.com>
2021-07-29 14:54:54 +0200

    added main for exception class

--- src/main/java/vending/MainExceptionalMachine.java
+++ src/main/java/vending/MainExceptionalMachine.java
@@ -1,5 +1,24 @@
 package vending;
 
-public class MainExceptionalMachine {
+import vending.productException.*;
+import vending.ExceptionalVendingMachine.ProductNotFoundException;
 
+public class MainExceptionalMachine {
+    public static void main(String[] args) throws ProductNotFoundException {
+        try {
+            Chocolate chocolate = new Chocolate();
+            SaltySnack saltySnack = new SaltySnack();
+            SoftDrink softDrink = new SoftDrink();
+            ExceptionalVendingMachine exceptionalVendingMachine = new ExceptionalVendingMachine(3, 6, 2);
+            exceptionalVendingMachine.addStock(chocolate, 4);
+            exceptionalVendingMachine.buy(softDrink);
+            exceptionalVendingMachine.buy(softDrink);
+            exceptionalVendingMachine.buy(softDrink);
+            System.out.println("There are "+exceptionalVendingMachine.getStock(chocolate)+ " in stock." );
+            System.out.println("There are " +exceptionalVendingMachine.getStock(saltySnack)+ " in stock.");
+            System.out.println("There are " +exceptionalVendingMachine.getStock(softDrink)+ " in stock.");
+        } catch (ProductNotFoundException ex) {
+            System.out.print(ex);
+        }
+    }
 }
